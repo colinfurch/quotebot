ed15456e6c6c9c2f06098d8d6d30e4732a640160
colinfurch <37372171+colinfurch@users.noreply.github.com>
2018-10-02 07:59:43 -0400

    Updated from Brython Server: 10/2/2018 7:59:42 AM

--- yeetquote.py
+++ yeetquote.py
@@ -1,4 +1,5 @@
 import random
+
 pn = ["Colin","Catrin","Carlton","Jackson","Celeste","Sara","Jessica","Aidan","Olivia","Jack","Ethan"]
 vb = ["is", "was", "shall be"]
 adj = ["gay", "godlike", "mega-gay"]
@@ -10,4 +11,6 @@ adjr = random.randint(0,2)
 sentrand = random.randint(0,2)
 
 print (pn[pnr], vb[vbr], adj[adjr])
+
+#please forget about this failed abortion of code
 
