32ac84a9588b0b50643a1abf9d1c9e118293e7e3
colinfurch <37372171+colinfurch@users.noreply.github.com>
2018-10-02 14:50:55 -0400

    Updated from Brython Server: 10/2/2018 2:50:54 PM

--- somerandomfuckeryofifthencodes.py
+++ somerandomfuckeryofifthencodes.py
@@ -1,3 +1,15 @@
 #alright, some basic input output practice
 print ("Alright, whats your name?")
-name = input ("Hi there ")
+name = input()
+print ("Hi there", name)
+print ('how are ya?')
+how = input()
+if how.lower() in ['yeet']:
+    print ('lemme guess,' +name+' you bring many gifts?')
+    gifts = input()
+    if gifts.lower() in ["yes", "yep", "yee","ye",'yeet']:
+        print ('alright, come on in')
+    
+else:
+    print ('alright. Nice meeting yah. Enjoy your drink')
+    input()
