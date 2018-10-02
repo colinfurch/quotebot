a8621ae922fc9b79ee9594c8142e88c87b5ad6d6
colinfurch <37372171+colinfurch@users.noreply.github.com>
2018-10-02 07:52:21 -0400

    Updated from Brython Server: 10/2/2018 7:52:20 AM

--- yeetquote.py
+++ yeetquote.py
@@ -1,1 +1,14 @@
 import random
+import ggame
+pn = ["Colin","Catrin","Carlton","Jackson","Celeste","Sara","Jessica","Aidan","Olivia","Jack","Ethan"]
+vb = ["is", "was", "shall be"]
+adj = ["gay", "godlike", "mega-gay"]
+
+pnr = random.randint(0,10)
+vbr = random.randint(0,2)
+adjr = random.randint(0,2)
+
+sentrand = random.randint(0,2)
+
+print (pn[pnr], vb[vbr], adj[adjr])
+
